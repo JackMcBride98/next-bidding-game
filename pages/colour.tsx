44863af4a7bb0246f603c04983bec2a722dfd5d0
Jack McBride <mcbride.jack1@gmail.com>
2022-08-25 14:17:49 +0100

    ADd secret Colour page

---
+++ pages/colour.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+interface Props {}
+
+const Colour: NextPage<Props> = () => {
+  return (
+    <div className="w-screen h-screen">
+      <div className="new"></div>
+    </div>
+  );
+};
+
+export default Colour;
