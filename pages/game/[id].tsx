6f76cce042703dcbcbbde0f48b22458a571aecff
Jack McBride <mcbride.jack1@gmail.com>
2023-01-22 11:39:58 +0000

    Import player model so that data fetching works correctly (debugged using vercel function logs)

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import RoundRow from '../../components/roundRow';
 import type { NextPage, GetServerSideProps } from 'next';
 import GameModel, { GameType } from '../../models/game';
-import { PlayerType } from '../../models/player';
+import PlayerModel, { PlayerType } from '../../models/player';
 import dbConnect from '../../lib/dbConnect';
 import Link from 'next/link';
 import Head from 'next/head';
